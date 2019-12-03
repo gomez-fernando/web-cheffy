81389086f9a804323e444b2bedb3ed230bee1ff6
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2019-12-03 13:30:43 +0100

    nuevos textos y mapa de Marbella

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,25 +21,23 @@
 	<nav class="menu">
         <a href="#">{{ __('lang.about_us') }}</a>
 		<a href="#">{{ __('lang.services') }}</a>
-		<a href="#">Contacto</a>
+		<a href="#">{{ __('lang.contact') }}</a>
 	</nav>
 
 	<main class="container">
 		<div class="row nosotros justify-content-center">
 			<div class="col-12 text-center">
-				<h2 class="subtitulo"><span>¿Quienes somos?</span></h2>
-				<h3 class="titulo">Nuestra pasión por la gastronomía y el alma de Marbella.</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officiis illum sequi quia doloribus quas consequatur autem inventore soluta voluptatum vero quibusdam illo cum, quod nostrum aspernatur. Autem, amet quasi.
-				</p>
-				<a href="#" class="enlace">Descubre tu próxima experiencia</a>
+				<h2 class="subtitulo"><span>{{ __('lang.who_are_we') }}</span></h2>
+				<h3 class="titulo">{{ __('lang.our_passion') }}</h3>
+				<p>{{ __('lang.text_1') }}</p>
+				<a href="#" class="enlace">{{ __('lang.discover') }}</a>
 			</div>
 		</div>
 
 		<div class="row productos">
 			<article class="col-12 text-center">
-				<h2 class="subtitulo"><span>Lo que ofrecemos</span></h2>
-				<p class="titulo">Nuestros Produtos</p>
+				<h2 class="subtitulo"><span>{{ __('lang.we_provide') }}</span></h2>
+				<p class="titulo">{{ __('lang.our_products') }}</p>
 				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit veniam saepe cum aspernatur neque odit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae deserunt perferendis. Lorem ipsum dolor sit amet consectetur.</p>
 			</article>
 
@@ -49,7 +47,7 @@
 						<figure class="producto">
 							<img src="img/products/pr-1.jpg" class="img-fluid" alt="">
 							<figcaption class="overlay">
-								<p class="overlay-texto">Descripción pequeña</p>
+								<p class="overlay-texto">Nombre del producto</p>
 							</figcaption>
 						</figure>
 					</article>
@@ -58,7 +56,7 @@
 						<figure class="producto">
 							<img src="img/products/pr-2.jpg" class="img-fluid" alt="">
 							<figcaption class="overlay">
-								<p class="overlay-texto">Descripción pequeña</p>
+								<p class="overlay-texto">Nombre del producto</p>
 							</figcaption>
 						</figure>
 					</article>
@@ -67,7 +65,7 @@
 						<figure class="producto">
 							<img src="img/products/pr-3.jpg" class="img-fluid" alt="">
 							<figcaption class="overlay">
-								<p class="overlay-texto">Descripción pequeña</p>
+								<p class="overlay-texto">Nombre del producto</p>
 							</figcaption>
 						</figure>
 					</article>
@@ -76,12 +74,12 @@
 						<figure class="producto">
 							<img src="img/products/pr-4.jpg" class="img-fluid" alt="">
 							<figcaption class="overlay">
-								<p class="overlay-texto">Descripción pequeña</p>
+								<p class="overlay-texto">Nombre del producto</p>
 							</figcaption>
 						</figure>
 					</article>
 
-					<button class="d-block btn-productos">Todos los productos</button>
+					<button class="d-block btn-productos">{{ __('lang.all_products') }}</button>
 				</div>
 			</div>
 		</div>
@@ -98,9 +96,9 @@
 					<img src="img/icons/icon-team.png" alt="">
 					<figcaption>
 						<p>
-							<strong class="mb-2">Un equipo de expertos</strong>
+							<strong class="mb-2">{{ __('lang.experts') }}</strong>
 							<div class="w-100"></div>
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem totam dolorem error.
+							{{ __('lang.team_made_up') }}
 						</p>
 					</figcaption>
 				</figure>
@@ -111,7 +109,7 @@
 					<img src="img/icons/icon-services.png" alt="">
 					<figcaption>
 						<p>
-							<strong class="mb-2">Una historia de servicio</strong>
+							<strong class="mb-2">{{ __('lang.commitment') }}</strong>
 							<div class="w-100"></div>
 							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem totam dolorem error.
 						</p>
@@ -138,11 +136,16 @@
 	<div class="container-fluid">
 		<section class="contacto row justify-content-center">
 			<div class="col-12 col-md-9 text-center">
-				<h2 class="subtitulo"><span>Contactanos</span></h2>
+				<h2 class="subtitulo"><span>{{ __('lang.where_are_we') }}</span></h2>
 			</div>
+			<iframe class="col-12 col-md-9" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1603.4677862691858!2d-4.895630342007969!3d36.50741729500249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfdc51b563715b018!2sPlaya%20Europa!5e0!3m2!1ses!2ses!4v1575375396733!5m2!1ses!2ses" width="100%" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 
-			<iframe class="col-12 col-md-9" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12656.165754243311!2d-99.16636547458687!3d19.425338392172087!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1568329227255!5m2!1ses-419!2smx" width="100%" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-
+			<div class="w-100 mb-4"></div>
+			<div>
+				<p class="border-bottom border-top">
+					<img src="img/icons/icon-cellphone.png" alt="">Playa La Fontanilla, s/n, 29602 Marbella, Málaga
+				</p>
+			</div>
 			<div class="w-100 mb-4"></div>
 			<div>
 				<p class="border-bottom border-top">
